ad11bab92fa849436bbf46f573f1f4c478a37314
HarmonyMurombo <harmonysvinurai1@gmail.com>
2025-08-12 00:37:08 +0200

    latest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,28 @@
-import type { Metadata } from "next";
-import { getServerSession } from "next-auth";
-import { authOptions } from "auth"; // the file we made earlier
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import Provider from "@/components/providers/SessionProvider";
+import type { Metadata } from 'next';
+
+import './globals.css'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: "VelosDrop | On-Demand Local Delivery",
-  description: "Fast and reliable delivery of your goods",
-};
+  title: 'VelosDrop | On-Demand Local Delivery',
+  description: 'Fast and reliable delivery of your goods, ',
+}
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  const session = await getServerSession(authOptions);
-
   return (
     <html lang="en">
       <body>
-        <Provider session={session}>
-          <Navbar />
-          <main className="relative overflow-hidden">
-            {children}
-          </main>
-          <Footer />
-        </Provider>
+        <Navbar />
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
